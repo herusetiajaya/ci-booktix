b55d127aae48d240012520a796ea33a837d1dc95
herusetiajaya <setyaone27@gmail.com>
2023-12-01 22:55:07 +0700

    initial commit

---
+++ application/views/about/index.php
@@ -0,0 +1,27 @@
+<div class="container mt-3 mb-3">
+    <div class="card-header bg-gradient-warning">
+        <h3 class="text text-white">About Us</h3>
+        <div class="row mt-3 text-dark">
+            <div class="col m6 light">
+                <h5>Booktix adalah</h5>
+                <p>Aplikasi pemesanan tiket film secara online dan Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores dicta possimus voluptatibus ad! Dignissimos fuga alias similique esse aperiam accusantium officiis nihil accusamus, amet doloribus qui suscipit nam sequi modi sed maxime magni labore nisi? Ea, quidem doloremque corporis deserunt, perspiciatis obcaecati quis at voluptas esse, quasi fugiat quas dolor.</p>
+            </div>
+            <div class="col m6 light text-white">
+                <div class="slider row justify-content-center">
+                    <img src="<?= base_url('assets/'); ?>img/profile/default.png" class="rounded-circle img-thumbnail img-slideshow" style="width: 20%;">
+                    <img src="<?= base_url('assets/'); ?>img/profile/admin.png" class="rounded-circle img-thumbnail img-slideshow" style="width: 20%;">
+                    <img src="<?= base_url('assets/'); ?>img/profile/martis.png" class="rounded-circle img-thumbnail img-slideshow" style="width: 20%;">
+                </div>
+            </div>
+        </div>
+        <hr class="my-4 badge-dark">
+        <h6>CodeIgniter 3.10</h6>
+        <h6>PHP 8.2.4</h6>
+        <h6>Bootstrap 4.3.1</h6>
+        <h6>SweetAlert 2.11.7</h6>
+        <h6>sb-admin2</h6>
+        <h6>WPU</h6>
+        <p class="lead"></p>
+        <!-- <hr class="my-4 badge-dark"> -->
+    </div>
+</div>
